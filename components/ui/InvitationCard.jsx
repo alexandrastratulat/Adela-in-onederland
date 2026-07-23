c9f4c7872b91657caf9d3643f37f9c3ee6c23a1b
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 17:51:03 +0300

    Add InvitationCard component for event details

---
+++ components/ui/InvitationCard.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import styles from "./InvitationCard.module.css";
+
+export default function InvitationCard(){
+
+return(
+
+<div className={styles.wrapper}>
+
+<h1>
+
+Adela in ONEderland
+
+</h1>
+
+<p>
+
+Capitolul I
+
+</p>
+
+<div className={styles.line}/>
+
+<h2>
+
+28 noiembrie 2026
+
+</h2>
+
+<h3>
+
+Ora 13:00
+
+</h3>
+
+<p>
+
+Vila Hepa
+
+</p>
+
+<p>
+
+Brașov
+
+</p>
+
+</div>
+
+);
+
+}
