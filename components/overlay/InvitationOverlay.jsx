6ab84f66040ecdd0dbf0b9d9714ae8a2f2bc932f
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 18:00:01 +0300

    Refactor InvitationOverlay component structure

--- components/overlay/InvitationOverlay.jsx
+++ components/overlay/InvitationOverlay.jsx
@@ -2,49 +2,54 @@
 
 import { motion } from "framer-motion";
 
-import InvitationCard from "../ui/InvitationCard";
+import InvitationCard from "./InvitationCard";
+import Countdown from "./Countdown";
 
-export default function InvitationOverlay(){
+export default function InvitationOverlay() {
 
-    return(
+    return (
 
         <motion.div
 
-            initial={{
-
-                opacity:0
-
-            }}
-
-            animate={{
-
-                opacity:1
-
-            }}
-
-            exit={{
-
-                opacity:0
-
-            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
 
             transition={{
 
-                duration:1
+                duration: 1.5
 
             }}
 
             style={{
 
-                position:"absolute",
-                inset:0,
-                pointerEvents:"none"
+                position: "absolute",
+                inset: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                pointerEvents: "none"
 
             }}
 
         >
 
-            <InvitationCard/>
+            <div
+                style={{
+
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "35px"
+
+                }}
+            >
+
+                <InvitationCard />
+
+                <Countdown />
+
+            </div>
 
         </motion.div>
 
