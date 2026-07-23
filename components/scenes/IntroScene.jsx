278d651cb6a86b0ce67456f06b2e8568191197c1
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 17:07:55 +0300

    Add IntroScene component with Atmosphere, Desk, and Book

---
+++ components/scenes/IntroScene.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Desk from "../world/Desk";
+import Book from "../world/Book";
+import Atmosphere from "../world/Atmosphere";
+
+export default function IntroScene() {
+
+    return(
+
+        <>
+
+            <Atmosphere/>
+
+            <Desk/>
+
+            <Book/>
+
+        </>
+
+    )
+
+}
