52fc7d3cc72799ca4f6c1cd105b2e58746eea16c
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 17:31:01 +0300

    Refactor Experience component structure and lighting

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,35 +1,71 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
+
+import {
+
+Environment
+
+} from "@react-three/drei";
+
 import CameraRig from "./camera/CameraRig";
+
 import IntroScene from "./scenes/IntroScene";
 
-export default function Experience({ started }) {
-  return (
-    <Canvas
-      shadows
-      dpr={[1, 2]}
-      camera={{
-        position: [0, 1.6, 8],
-        fov: 38
-      }}
-    >
-      <color attach="background" args={["#f8f4ed"]} />
+export default function Experience({
 
-      <ambientLight intensity={0.5} />
+started
 
-      <directionalLight
-        castShadow
-        position={[4, 8, 5]}
-        intensity={2}
-      />
+}){
 
-      <Environment preset="sunset" />
+return(
 
-      <CameraRig />
+<Canvas
 
-      <IntroScene started={started} />
-    </Canvas>
-  );
+shadows
+
+camera={{
+
+position:[0,1.6,8],
+
+fov:38
+
+}}
+
+>
+
+<color
+
+attach="background"
+
+args={["#f7f4ef"]}
+
+/>
+
+<ambientLight intensity={0.45}/>
+
+<directionalLight
+
+castShadow
+
+position={[4,8,4]}
+
+intensity={2}
+
+/>
+
+<Environment preset="sunset"/>
+
+<CameraRig/>
+
+<IntroScene
+
+started={started}
+
+/>
+
+</Canvas>
+
+);
+
 }
