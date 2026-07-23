2bc8fc09fe642786a35fbc73afa68df63d4238b2
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 16:53:10 +0300

    Refactor Book component with GSAP animations

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,37 +1,90 @@
+import { useRef } from "react";
 import { Float } from "@react-three/drei";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-export default function Book(){
+export default function Book() {
 
-return(
+  const frontCover = useRef();
+  const pages = useRef();
 
-<Float
+  useGSAP(() => {
 
-speed={2}
+    const tl = gsap.timeline({
+      repeat: -1,
+      repeatDelay: 6
+    });
 
-rotationIntensity={0.2}
+    tl.to(frontCover.current.rotation, {
+      y: -Math.PI * 0.82,
+      duration: 2.2,
+      ease: "power3.inOut"
+    });
 
-floatIntensity={0.5}
+    tl.to(pages.current.rotation, {
+      y: -0.08,
+      duration: 1,
+      ease: "power2.out"
+    }, "-=1.4");
 
->
+    tl.to({}, { duration: 3 });
 
-<mesh>
+    tl.to(frontCover.current.rotation, {
+      y: 0,
+      duration: 1.8,
+      ease: "power3.inOut"
+    });
 
-<boxGeometry
+    tl.to(pages.current.rotation, {
+      y: 0,
+      duration: 1
+    }, "-=1.4");
 
-args={[3.5,4.8,0.35]}
+  });
 
-/>
+  return (
 
-<meshStandardMaterial
+    <Float
+      speed={1.4}
+      rotationIntensity={0.15}
+      floatIntensity={0.3}
+    >
 
-color="#f6e8dc"
+      <group>
 
-/>
+        {/* Coperta spate */}
 
-</mesh>
+        <mesh position={[0,0,-0.12]}>
+          <boxGeometry args={[3.8,5.2,0.12]} />
+          <meshStandardMaterial color="#F5E6D8"/>
+        </mesh>
 
-</Float>
+        {/* Paginile */}
 
-);
+        <mesh ref={pages}>
+          <boxGeometry args={[3.55,4.95,0.18]} />
+          <meshStandardMaterial color="#FFFDF8"/>
+        </mesh>
+
+        {/* Coperta față */}
+
+        <group ref={frontCover} position={[-1.9,0,0]}>
+
+          <mesh position={[1.9,0,0.12]}>
+            <boxGeometry args={[3.8,5.2,0.12]} />
+            <meshStandardMaterial
+              color="#EFD8C7"
+              metalness={0.05}
+              roughness={0.7}
+            />
+          </mesh>
+
+        </group>
+
+      </group>
+
+    </Float>
+
+  );
 
 }
