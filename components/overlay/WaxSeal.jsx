51e2bfda5c86263a974c4c0335486f85538ea551
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 18:05:35 +0300

    Add WaxSeal component with basic structure

---
+++ components/overlay/WaxSeal.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styles from "./WaxSeal.module.css";
+
+export default function WaxSeal(){
+
+return(
+
+<div className={styles.wrapper}>
+
+<div className={styles.seal}>
+
+A
+
+</div>
+
+</div>
+
+);
+
+}
