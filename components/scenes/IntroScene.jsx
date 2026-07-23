25c40ca6f507f0b22115082279ddbf28a63c0897
Alexandra Stratulat <48979881+alexandrastratulat@users.noreply.github.com>
2026-07-23 17:33:54 +0300

    Refactor IntroScene to include FloatingDust
    Added FloatingDust component to IntroScene.

--- components/scenes/IntroScene.jsx
+++ components/scenes/IntroScene.jsx
@@ -1,17 +1,37 @@
 "use client";
 
 import Atmosphere from "../world/Atmosphere";
+
 import Desk from "../world/Desk";
+
 import Book from "../world/Book";
 
-export default function IntroScene({ started }) {
+import FloatingDust from "../effects/FloatingDust";
 
-    return (
-        <>
-            <Atmosphere />
-            <Desk />
-            <Book started={started} />
-        </>
-    );
+export default function IntroScene({
+
+started
+
+}){
+
+return(
+
+<>
+
+<Atmosphere/>
+
+<Desk/>
+
+<Book
+
+started={started}
+
+/>
+
+<FloatingDust/>
+
+</>
+
+);
 
 }
